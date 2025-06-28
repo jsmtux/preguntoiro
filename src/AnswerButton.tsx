2bd2338b68d7230caabb5b4dbb13a36c572a0bef
jsmtux <jsmtux@gmail.com>
2025-06-28 12:51:04 +0200

    Update logo and buttons

--- src/AnswerButton.tsx
+++ src/AnswerButton.tsx
@@ -8,9 +8,9 @@ export function AnswerButton({ text, name, delay }) {
     }
     setTimeout(() => setBtnOpacity(1.0), delay);
   return (
-    <div style={style} class="transition-opacity duration-500 bg-gradient-to-t from-[#f59e0b] via-[#fcd34d] to-[#fef9c3] shadow-lg shadow-yellow-500/50 p-8 max-w items-center gap-y-4 rounded-xl shadow-lg outline-4 -outline-offset-1 outline-yellow-500">
+    <div style={style} class="backdrop-hue-rotate-180 backdrop-saturate-200 backdrop-blur-xs backdrop-brightness-200 transition-opacity duration-500 bg-gradient-to-t from-[#f59f0b4e] via-[#fcd34d46] to-[#fef9c35d] shadow-lg shadow-yellow-500/50 p-8 max-w items-center gap-y-4 rounded-xl shadow-lg outline-4 -outline-offset-1 outline-yellow-500">
         <div>
-            <div class="p-8 text-4xl font-medium text-blue-900">{name}. {text}</div>
+            <div class="p-8 text-4xl font-medium text-blue-200">{name}. {text}</div>
         </div>
     </div>
 );
--- src/QuestionTitle.tsx
+++ src/QuestionTitle.tsx
@@ -8,11 +8,9 @@ export function QuestionTitle({title, index}) {
     setTimeout(() => setBtnOpacity(1.0), 10);
   return (
     <div class="place-items-center">
-        <div style={style} class="bg-gradient-to-t from-[#0e7490] via-[#3b82f6] to-[#4f46e5] duration-500 shadow-blue-500/50 p-4 items-center gap-y-4 rounded-xl shadow-lg bg-orange-800 outline-4 -outline-offset-1 outline-blue-500">
-            <div>
-                <p class="text-gray-500 dark:text-gray-400">Pregunta {index}</p>
-                <div class="p-4 text-5xl font-medium text-black dark:text-white">{title}</div>
-            </div>
+        <div style={style} class="backdrop-saturate-200 backdrop-blur-xs backdrop-brightness-150 bg-gradient-to-t from-[#0e74903d] via-[#3b82f63d] to-[#4e46e53d] duration-500 shadow-blue-500/50 p-4 items-center gap-y-4 rounded-xl shadow-lg outline-4 -outline-offset-1 outline-blue-500">
+            <p class="text-gray-500 dark:text-gray-400 text-2xl">Pregunta {index}</p>
+            <div class="p-4 text-5xl font-medium text-black dark:text-white">{title}</div>
         </div>
     </div>
 );
